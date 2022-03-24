61a720ecc7c2cf460b9f51fe81a225f5f73599af
HenokB <henokb2124@gmail.com>
2022-03-25 00:21:59 +0300

    variables and input

---
+++ 0x002/002_simple program.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+/*
+	- A simple program to add two numbers
+	- Learn how to declare variables
+	- Learn how to take an input
+
+
+*/
+
+func main() {
+    var firstNumber int=0
+    var secondNumber int=0
+    var result int=0
+    
+    fmt.Printf("Enter first number: ")
+    fmt.Scanf("%d",&firstNumber)
+    
+    fmt.Printf("Enter number2: ")
+    fmt.Scanf("%d",&secondNumber)
+    
+    result = firstNumber & secondNumber
+    fmt.Printf("Result: %d",result)
+}
