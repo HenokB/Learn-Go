7b9a6b2c57e1b38236f52950b78f6443b093e896
HenokB <henokb2124@gmail.com>
2022-03-26 00:34:50 +0300

    variables

---
+++ 0x002/variables.go
@@ -0,0 +1,25 @@
+/*
+- variable declaration
+- constant declaration
+*/
+
+package main
+ 
+import "fmt"
+ 
+func main () {
+ 
+var num int = 3 // variable declaration
+ 
+var (
+
+x = 4
+y = 6 //declaration of multiple variables
+)
+ 
+seven := 7 // easy shorthand declaration of variables
+ 
+const pi float64 = 3.14272345 //constants declaration
+ 
+var name string = "Learn Golang" //string declaration
+}
